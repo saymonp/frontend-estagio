c4fb5bfe4e4146c54bc9c919bb632f7e132b4152
saymonp <saymon.t@outlook.com>
2021-04-29 16:38:17 -0300

    Order upload de arquivos

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class OrderComponent implements OnInit {
   focus: any;
   focus1: any;
+  public filesToUpload = [];
 
   constructor() { }
 
@@ -18,5 +19,38 @@ export class OrderComponent implements OnInit {
     console.log($element);
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
+
+  createOrder() {
+    this.filesToUpload.map((file) => {
+      console.log(file)
+      // Upload
+    })
+
+    // Create Order
+  }
+
+  removeFile(index) {
+    this.filesToUpload.splice(index, 1);
+  }
+
+  uploadImage(event) {
+    const fileList = event.target.files;
+
+
+    for (const file of fileList) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+      const that = this;
+      reader.onload = function () {
+        that.filesToUpload.push({"path": "orders/", "fileName":file.name, "data": reader.result})
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };
+    };
+      console.log(this.filesToUpload)
+  }
+
+}
 
 }
--- src/app/pages/productsList/productsList.component.ts
+++ src/app/pages/productsList/productsList.component.ts
@@ -11,6 +11,7 @@ export class ProductsListComponent implements OnInit {
   focus1: any;
   doubleSlider = [20, 60];
   state_default: boolean = true;
+  public filesToUpload = [];
 
   constructor() { }
 
@@ -20,5 +21,38 @@ export class ProductsListComponent implements OnInit {
     console.log($element);
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
+
+  createOrder() {
+    this.filesToUpload.map((file) => {
+      console.log(file)
+      // Upload
+    })
+
+    // Create Order
+  }
+
+  removeFile(index) {
+    this.filesToUpload.splice(index, 1);
+  }
+
+  uploadImage(event) {
+    const fileList = event.target.files;
+
+
+    for (const file of fileList) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+      const that = this;
+      reader.onload = function () {
+        that.filesToUpload.push({"path": "orders/", "fileName":file.name, "data": reader.result})
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };
+    };
+      console.log(this.filesToUpload)
+  }
+
+}
 
 }
